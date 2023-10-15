068fcd95c80a4263f4a0d25e850f3390b0cd840c
Hassan Naseer <139218360+Hassan-cyber-dot@users.noreply.github.com>
2023-10-15 07:34:59 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+console.log(chalk.blue("try a number between 1 to 10"));
+let actualNumber = 9;
+let NumTries = 3;
+while (NumTries > 0) {
+    const answers = await inquirer.prompt([
+        {
+            name: "YourGuess",
+            type: "number",
+            message: "Enter a number",
+        },
+    ]);
+    console.log(answers);
+    if (answers.YourGuess == actualNumber) {
+        console.log(chalk.blue("hurray,You guessed the right number"));
+        NumTries == 0;
+    }
+    else {
+        console.log(chalk.blue("sorry,You guessed the wrong number"));
+    }
+    if (actualNumber > answers.YourGuess) {
+        console.log(chalk.blue("Think about a Higher number"));
+    }
+    else {
+        console.log(chalk.blue("Think about a Lower number"));
+    }
+    console.log(chalk.blue(`You have ${NumTries - 1} left`));
+    NumTries--;
+}
